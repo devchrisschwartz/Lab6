0ef5659eaf7272815c06247884b6b81c10cd8b79
chris <tarancello@gmail.com>
2018-07-24 16:06:44 -0400

    fixing random number method range

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -39,7 +39,7 @@ namespace Lab6
         {
             Random rndNum = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber));
 
-            int rnd = rndNum.Next(Low, High);
+            int rnd = rndNum.Next(Low, High+1);
 
             return rnd;
         }
